d2f274610d82b6ccccbe58cd00903ffee4c0251b
ouyangfan <ouyangfan1991@gmail.com>
2017-09-10 00:30:39 +0800

    get all goods from fansdroid domain

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ import networkUtil from '/util/networkUtil.js';
 App({
   onLaunch: function () {
     console.log('App Launch')
-    var that = this;
+    var self = this;
     var url = 'https://shmall.fansdroid.net/get/token'
     networkUtil._get(url,
       function (res) {
-        that.globalData.token = res.data;
-        console.log("token = " + that.globalData.token);
-       // that.getAllGoods();   // 异步获取商品
+        self.globalData.token = res.data;
+        console.log("token = " + self.globalData.token);
+        self.getAllGoods();   // 异步获取商品
       },
       function (res) {
         console.log("fail , res data= " + res.data + ", url = " + url);
-        this.updateToken();
+        self.updateToken();
         wx.showToast({
           title: '获取token失败' + res.data,
         })
@@ -32,22 +32,18 @@ App({
     token: null,
 
     allGoods: [],       // 所有的商品
-    allGoodsSize: 0,    // 商品总数
-    goodsPageIndex: 1,  // 请求时商品分页
-    goodsPageSize: 50,  // 请求时商品每页总数
-    getedGoodsSize: 0,  // 已经拿到的商品总数
   },
 
   // 更新token
   updateToken: function () {
     console.log('update Token');
-    var that = this;
+    var self = this;
     var updateTokenUrl = 'https://shmall.fansdroid.net/update/token'
     // var updateTokenUrl = 'https://api.vdian.com'
     networkUtil._get(updateTokenUrl,
       function (res) {
-        that.globalData.token = res.data;
-        console.log("token = " + that.globalData.token);
+        self.globalData.token = res.data;
+        console.log("token = " + self.globalData.token);
       },
       function (res) {
         wx.showToast({
@@ -60,47 +56,19 @@ App({
    * 获取商店内所有商品
    */
   // url = https://api.vdian.com/api?param={"page_num":1,"page_size":10,"orderby":1,"update_start":"2012-11-12 16:36:08","update_end":"2015-11-12 16:36:08","status":1}&public=   {"method":"vdian.item.list.get","access_token":"40be967eabb8057fc7975ed64895b5d900023716b1","version":"1.0","format":"json"}
+  // url = https://shmall.fansdroid.net/get/allgoods
   getAllGoods: function () {
     var self = this;
-    var allGoodsUrl = 'https://api.vdian.com/api?param={"page_num":' + self.globalData.goodsPageIndex + ',"page_size":' + self.globalData.goodsPageSize + ',"orderby":1}&public={"method":"vdian.item.list.get","access_token":"' + self.globalData.token + '","version":"1.0","format":"json"}';
+    var allGoodsUrl = "https://shmall.fansdroid.net/get/allgoods";
     // console.log("allGoodsUrl = " + allGoodsUrl);
     networkUtil._get(allGoodsUrl,
       function (res) {
-        let currentPageSize = res.data.result['item_num'];
-        self.globalData.allGoodsSize = res.data.result['total_num'];
-        self.globalData.getedGoodsSize = self.globalData.getedGoodsSize + currentPageSize;
-        // console.log("Goods server total num = " + res.data.result['total_num'] + ", allGoodSize = " + self.globalData.allGoodsSize + " , currentPageSize = " + currentPageSize + " , getedGoodsSize = " + self.globalData.getedGoodsSize);
-
-        let pageItems = res.data.result['items']
-        for (let i = 0; i < pageItems.length; i++) {
-          // console.log('pageItems = ' + pageItems[i]['item_name'] + " , cateId = " + pageItems[i]['cates'][0]['cate_id']);
-          let cates = pageItems[i]['cates']
-          for (let j = 0; j < cates.length; j++) {
-            // console.log('pageItems name = ' + pageItems[i]['item_name'] + " , cateId = " + cates[j]['cate_id']);
-
-            var good = Good.createNew();
-            good.cate_id = cates[j]['cate_id'];
-            good.itemid = pageItems[i]['itemid'];
-            good.item_desc = pageItems[i]['item_desc'];
-            good.item_name = pageItems[i]['item_name'];
-            good.imgs = pageItems[i]['imgs'];
-            good.price = pageItems[i]['price'];
-
-            self.globalData.allGoods.push(good);
-          }
-        }
-
-        if (self.globalData.allGoodsSize - self.globalData.getedGoodsSize > 0) {
-          console.log("please get goods continue! ");
-          self.globalData.goodsPageIndex++;
-          self.getAllGoods();
-        } else {
-          console.log("no more get goods! ");
-        };
-      
+        self.globalData.allGoods = res.data.result;
+        console.log("all goods size = " + self.globalData.allGoods.length);
+        // self.printAllGoods();
       },
       function (res) {
-        console.log("category fail data = " + res.data);
+        console.log("all goods fail data = " + res.data);
       });
   },
 
@@ -112,22 +80,3 @@ App({
     }
   }
 })
-
-/**
- * 定义一个商品类
- * 字段根据微店网络api返回提取
- * 注意：商品分类cate_id,同一个商品可能有不同的cate_id,
- * 此时把它当成不同商品处理，主要是为了后面好根据cate_id拿数据
- */
-var Good = {
-  createNew: function () {
-    var good = {};
-    good.cate_id = null;    // 商品分类id
-    good.itemid = null;    // 单个商品id
-    good.item_desc = null;  // 商品描述
-    good.item_name = null;  // 商品名称
-    good.imgs = [];         // 商品图片
-    good.price = 0;         // 商品价格
-    return good;
-  }
-};
